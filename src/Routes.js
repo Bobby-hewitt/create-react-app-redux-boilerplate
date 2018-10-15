642b03fc7a7843323f15633e51d9526f8b0c076a
Bobby Hewitt <bobby.hewitt@w12studios.com>
2018-10-15 14:54:45 +0100

    remove counter

--- src/Routes.js
+++ src/Routes.js
@@ -6,16 +6,12 @@ import About from './containers/About'
 import styles from './App.css'
 
 const Routes = () => (
-  <div style={{padding:'20px'}}>
-    <header>
-      <Link style={{paddingRight: '10px'}}to="/">Home</Link>
-      <Link style={{paddingRight: '10px'}}to="/about">About</Link>
-    </header>
-
+  <div>
+   
     <main>
       <div className="container">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </div>
     </main>
   </div>
